fd1edc08d6adb041e4e2b604dc30adddd09cf0c0
Niklas Rosencrantz <niklasr@protonmail.com>
2023-09-02 12:51:15 +0200

    Delete sample-app/setup/database directory

--- sample-app/setup/database/3-roles.sql
+++
@@ -1,7 +0,0 @@
--- Copyright (c) HashiCorp, Inc.
--- SPDX-License-Identifier: MPL-2.0
-
-CREATE ROLE vault_db_user LOGIN SUPERUSER PASSWORD 'vault_db_password';
-CREATE ROLE readonly NOINHERIT;
-
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO "readonly";
