867274d22daae9556acdd33f210bf8ea17ee3bbc
Niklas Rosencrantz <niklasr@protonmail.com>
2023-09-02 19:06:34 +0200

    Update run.sh

--- sample-app/run.sh
+++ sample-app/run.sh
@@ -1,8 +1,2 @@
-#!/bin/sh
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
-
-# (re)start application and its dependencies
 docker compose down --volumes
 docker compose up -d --build
